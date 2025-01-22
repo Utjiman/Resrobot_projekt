31907a4fae2d480b67ca852b4f7187c3e49210f2
Richard Bodén <utjis@hotmail.com>
2025-01-22 14:39:55 +0100

    Created show departure from stop function and tested it in test_timetable.py.

--- backend/time_table.py
+++ backend/time_table.py
@@ -1,9 +1,25 @@
-
+from backend.connect_to_api import ResRobot
 
 class TimeTable:
 
-    def show_departure_from_stop():
-        pass
+    def __init__(self, resrobot: ResRobot):
+        self.resrobot = resrobot
+
+    def show_departure_from_stop(self, stop_id: int):
+        """
+        Show departures from a specific stop
+        """
+        data = self.resrobot.timetable_departure(location_id=stop_id)  # Gets data from resrobot
+        departures = data.get("Departure", [])
+        
+        result = []
+        for departure in departures:
+            line = departure.get("ProductAtStop", {}).get("name", "Okänd linje")
+            direction = departure.get("direction", "Okänd destination")
+            time = departure.get("time", "N/A")
+            result.append({"Linje": line, "Destination": direction, "Avgångstid": time})
+
+        return result
 
 
 
@@ -26,8 +42,3 @@ class TimeTable:
     
     def show_one_hour_ahead():
         pass
-
-
-
-
-
