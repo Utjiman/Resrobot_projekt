b8cb63af8d3240d41f1a2e8f902d99dbcb08b90c
Richard Bodén <utjis@hotmail.com>
2025-01-22 11:07:47 +0100

    Create timetable.py with function

---
+++ backend/time_table.py
@@ -0,0 +1,33 @@
+
+
+class TimeTable:
+
+    def show_departure_from_stop():
+        pass
+
+
+
+
+   
+    def filter_stop():
+       pass
+
+    
+    
+    
+    
+    def show_time_to_departure():
+        pass
+
+
+    
+    
+    
+    
+    def show_one_hour_ahead():
+        pass
+
+
+
+
+
