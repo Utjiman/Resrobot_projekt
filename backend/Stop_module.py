b204cb6280e4125f5285d2837d84a069aeb3d0a8
Richard Bodén <utjis@hotmail.com>
2025-01-23 15:49:48 +0100

    Created stop_module with some basic functions

---
+++ backend/Stop_module.py
@@ -0,0 +1,29 @@
+
+
+
+
+class Stops:
+    def __init__(self):
+        pass
+    
+    
+    def Search_stop_by_name():
+        pass
+
+    
+    
+
+    def Get_stop_info():
+        pass
+    
+    
+    
+    
+    def Find_nearby_stops():
+        pass
+
+
+    
+
+    
+
