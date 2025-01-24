70a3f0fd6334f089fe3b598d53d2d7dd30290f8c
Richard Bodén <utjis@hotmail.com>
2025-01-24 10:48:17 +0100

    fix 5

--- backend/Stop_module.py
+++ backend/Stop_module.py
@@ -1,29 +1,12 @@
-
-
-
-
 class Stops:
     def __init__(self):
         pass
-    
-    
+
     def Search_stop_by_name():
         pass
 
-    
-    
-
     def Get_stop_info():
         pass
-    
-    
-    
-    
+
     def Find_nearby_stops():
         pass
-
-
-    
-
-    
-
