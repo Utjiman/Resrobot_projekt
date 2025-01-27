b73f57a1e53d4ecacab3be11d5cd107352d7fb01
Richard Bodén <utjis@hotmail.com>
2025-01-27 12:55:58 +0100

    get stop info and fin nearby stop functions. not testet yet

--- backend/Stop_module.py
+++ backend/Stop_module.py
@@ -1,12 +1,42 @@
+from backend.connect_to_api import ResRobot
+
+
 class Stops:
-    def __init__(self):
-        pass
+    def __init__(self, resrobot: ResRobot):
+        self.resrobot = resrobot
 
     def Search_stop_by_name():
         pass
 
-    def Get_stop_info():
-        pass
+    def get_stop_info(self, ext_id: str):
+        """
+        Gets info about a specific stop
+        """
+        data = self.resrobot.access_id_from_location(ext_id)
+        stop_data = data.get("stopLocationOrCoordLocation", [])
+        if stop_data:
+            stop_details = next(iter(stop_data[0].values()))
+            return {
+                "name": stop_details.get("name"),
+                "lat": stop_details.get("lat"),
+                "lon": stop_details.get("lon"),
+                "products": stop_details.get("products"),
+            }
+        return None
 
-    def Find_nearby_stops():
-        pass
+    def find_nearby_stops(self, lat: float, lon: float, radius: int = 1000):
+        """
+        Finds nearby stops based on coordinates and radius
+        """
+        data = self.resrobot.get_nearby_stops(lat, lon, radius)
+        nearby_stops = [
+            {
+                "name": stop["StopLocation"]["name"],
+                "lat": stop["StopLocation"]["lat"],
+                "lon": stop["StopLocation"]["lon"],
+                "dist": stop["StopLocation"]["dist"],
+                "products": stop["StopLocation"]["products"],
+            }
+            for stop in data.get("stopLocationOrCoordLocation", [])
+        ]
+        return nearby_stops
